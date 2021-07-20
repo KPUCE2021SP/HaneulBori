df74cc00b5eac8ba5cca1eb004b036577d6fc118
권태우KwonTaeWoo <kwontaewoo0901@gmail.com>
2021-07-21 00:20:00 +0900

    폴더 추가

---
+++ taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  taewoo_swift_HaneulBori
+//
+//  Created by 권태우 on 2021/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
