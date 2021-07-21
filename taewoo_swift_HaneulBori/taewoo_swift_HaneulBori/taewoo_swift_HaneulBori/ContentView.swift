7453979bc86a7d132342414a336a9110e5c868a3
권태우KwonTaeWoo <kwontaewoo0901@gmail.com>
2021-07-21 16:31:44 +0900

    add LogIn ContentView

---
+++ taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  taewoo_swift_HaneulBori
+//
+//  Created by 권태우 on 2021/07/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var id = ""
+    @State var pw = ""
+
+    var body: some View {
+        NavigationView{
+            VStack {
+                Text("Hello, Log In To Use!")
+                    .font(.largeTitle)
+                Spacer()
+                TextField("TYPE ID HERE", text : $id)
+                    .padding([.top, .leading, .trailing])
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                TextField("TYPE PASSWORD HERE", text : $pw)
+                    .padding(.horizontal)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+//                NavigationLink(destination: MainContentView()
+//                                .navigationBarHidden(true)
+//                                .navigationBarBackButtonHidden(true)){
+//                    Text("LOG IN")
+//                        .foregroundColor(Color.white)
+//                        .padding()
+//                }
+                .border(Color.black)
+                .background(Color.black)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
