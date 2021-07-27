1907c2e910b346e6100c8364bdc5bd2719dac83f
권태우KwonTaewoo <kwontaewoo0901@gmail.com>
2021-07-27 15:45:44 +0900

    Add revenue display button

--- taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/AdminContentView.swift
+++ taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/AdminContentView.swift
@@ -8,27 +8,61 @@
 import SwiftUI
 
 struct AdminContentView: View {
+    @State var btnPressed = false
+    @State var money = 1000000
+    
     var body: some View {
-        VStack {
+        NavigationView{
             VStack {
                 VStack {
-                    Text("1번 세탁기")
-                    Text("(세탁기 상태)")
+                    VStack {
+                        Text("1번 세탁기")
+                        Text("(세탁기 상태)")
+                    }
+                    .foregroundColor(.green)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(Color.black)
+                    VStack {
+                        Text("2번 세탁기")
+                        Text("(세탁기 상태)")
+                    }
+                    .foregroundColor(.blue)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(Color.black)
+                }.padding()
+                Spacer()
+                NavigationLink(destination: SignUpContentView()){
+                    Text("MANAGE WASHING MACHINES")
+                        .foregroundColor(Color.white)
+                        .padding()
                 }
-                .foregroundColor(.green)
-                .multilineTextAlignment(.center)
-                .padding()
+                .frame(width: 300)
                 .border(Color.black)
-                VStack {
-                    Text("2번 세탁기")
-                    Text("(세탁기 상태)")
+                .background(Color.black)
+                Button(action: {btnPressed.toggle()}) {
+                    if btnPressed{
+                        Text("I earned \(money) (KRW)")
+                            .foregroundColor(Color.white)
+                            .padding()
+                    }
+                    else{
+                        Text("$how Me the Money")
+                            .foregroundColor(Color.white)
+                            .padding()
+                    }
+
                 }
-                .foregroundColor(.blue)
-                .multilineTextAlignment(.center)
-                .padding()
+                .frame(width: 300)
                 .border(Color.black)
-            }.padding()
-            Spacer()
+                .background(Color.black)
+                Spacer()
+//                if btnPressed{
+//                    Text("I earned $\(money)")
+//                }
+//                Spacer()
+            }
         }
     }
 }
