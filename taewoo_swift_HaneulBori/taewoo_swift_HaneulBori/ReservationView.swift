53e6ff751c1ebe8f2f4ba90feb12b208e3fdd769
권태우KwonTaewoo <kwontaewoo0901@gmail.com>
2021-07-28 14:38:57 +0900

    Add DatePicker on ReservationView

--- taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/ReservationView.swift
+++ taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/ReservationView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ReservationView: View {
+    @State var time = Date()
+    @State var isReserved = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Reserve To Use Washers")
+                .font(.title)
+            Spacer()
+            DatePicker(selection: $time, displayedComponents: .hourAndMinute){
+                Text("예약 시간")
+//                    .frame(width: 100.0)
+            }
+            .padding()
+            .frame(width: 380.0)
+//            .datePickerStyle(WheelDatePickerStyle())
+            Button(action: {isReserved = true}){    // Female
+                if isReserved{
+                    Text("RESERVE")
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .frame(width: 185)
+                        .border(Color.black)
+                        .background(Color.black)
+                        .opacity(0.5)
+                }
+                else{
+                    Text("RESERVE")
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .frame(width: 185)
+                        .border(Color.black)
+                        .background(Color.black)
+                }
+            }
+            Spacer()
+        }
     }
 }
 
