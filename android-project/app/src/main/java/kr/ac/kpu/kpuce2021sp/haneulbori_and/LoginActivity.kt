4ef487b8b34b03ebc36eff0caf93cae21e79981f
hkcho01 <hkcho01@naver.com>
2021-08-03 17:50:19 +0900

    UI Base

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
@@ -1,8 +1,10 @@
 package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity()
 {
@@ -11,10 +13,9 @@ class LoginActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val signInBtn=findViewById<Button>(R.id.signInBtn)
-        signInBtn.setOnClickListener(
-            var intent = Intent(applicationContext, MainActivity::class.java)
-        startActivity(intent)
-        )
+        signInBtn.setOnClickListener {
+            var intent= Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
@@ -1,11 +1,20 @@
 package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
+import android.app.AlertDialog
 import android.app.TabActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : TabActivity()
 {
+    companion object
+    {
+        var laundry1_reserved=false
+        var laundry2_reserved=false
+        var laundry3_reserved=false
+        var laundry4_reserved=false
+    }
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -24,6 +33,59 @@ class MainActivity : TabActivity()
 
 
         tabHost.currentTab=0
+
+
+        laundry1.setOnClickListener {
+            var dlg=AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("예약 시간")
+            dlg.setMessage("예약 시간들 리스트로 출력")
+            dlg.setIcon(R.mipmap.ic_launcher)
+            dlg.setNegativeButton("취소",null)
+            dlg.setPositiveButton("확인"){dialog,which->
+                laundry1_reserved=true
+                tv.text="내가 예약한 세탁기 번호 : 1번 / "+"시간 : "
+
+            }
+            dlg.show()
+        }
+        laundry2.setOnClickListener {
+            var dlg=AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("예약 시간")
+            dlg.setMessage("예약 시간들 리스트로 출력")
+            dlg.setIcon(R.mipmap.ic_launcher)
+            dlg.setNegativeButton("취소",null)
+            dlg.setPositiveButton("확인"){dialog,which->
+                laundry2_reserved=true
+                tv.text="내가 예약한 세탁기 번호 : 2번 / "+"시간 : "
+            }
+            dlg.show()
+        }
+        laundry3.setOnClickListener {
+            var dlg=AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("예약 시간")
+            dlg.setMessage("예약 시간들 리스트로 출력")
+            dlg.setIcon(R.mipmap.ic_launcher)
+            dlg.setNegativeButton("취소",null)
+            dlg.setPositiveButton("확인"){dialog,which->
+                laundry3_reserved=true
+                tv.text="내가 예약한 세탁기 번호 : 3번 / "+"시간 : "
+            }
+            dlg.show()
+        }
+        laundry4.setOnClickListener {
+            var dlg=AlertDialog.Builder(this@MainActivity)
+            dlg.setTitle("예약 시간")
+            dlg.setMessage("예약 시간들 리스트로 출력")
+            dlg.setIcon(R.mipmap.ic_launcher)
+            dlg.setNegativeButton("취소",null)
+            dlg.setPositiveButton("확인"){dialog,which->
+                laundry4_reserved=true
+                tv.text="내가 예약한 세탁기 번호 : 4번 / "+"시간 : "
+            }
+            dlg.show()
+        }
+
+
 
 
     }
