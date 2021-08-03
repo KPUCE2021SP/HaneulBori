722d4182141ed622397614063de224e3d56c1ff0
hkcho01 <hkcho01@naver.com>
2021-08-03 17:07:22 +0900

    First Commit

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
@@ -2,10 +2,19 @@ package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signInBtn=findViewById<Button>(R.id.signInBtn)
+        signInBtn.setOnClickListener(
+            var intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        )
     }
 }
--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
@@ -1,11 +1,30 @@
 package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
+import android.app.TabActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : TabActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var tabHost=this.tabHost
+
+        var tabSpecReserve=tabHost.newTabSpec("예약하기").setIndicator("예약하기")
+        tabSpecReserve.setContent(R.id.tabReserve)
+        tabHost.addTab(tabSpecReserve)
+
+        var tabSpecCancel=tabHost.newTabSpec("취소하기").setIndicator("취소하기")
+        tabSpecCancel.setContent(R.id.tabCancel)
+        tabHost.addTab(tabSpecCancel)
+
+
+        tabHost.currentTab=0
+
+
     }
 }
