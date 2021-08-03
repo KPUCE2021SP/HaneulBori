16eddae943ce455ed142ddd24db6dc5ac248a28e
hhh-one <904lhw@naver.com>
2021-08-03 16:49:40 +0900

    login activity를 첫 화면으로

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
@@ -1,11 +1,18 @@
 package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signInBtn.setOnClickListener {
+            var intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
