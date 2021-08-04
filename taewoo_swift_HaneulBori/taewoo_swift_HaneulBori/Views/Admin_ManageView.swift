8ceab356234d37c9fac7a932007e5f197e49f634
boifromangye <kwontaewoo0901@gmail.com>
2021-08-05 00:23:40 +0900

    Edit fix buttons on Admin_manageView again

--- taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/Views/Admin_ManageView.swift
+++ taewoo_swift_HaneulBori/taewoo_swift_HaneulBori/Views/Admin_ManageView.swift
@@ -14,71 +14,82 @@ struct Admin_ManageView: View {
 
     
     var body: some View {
-        VStack {
+        NavigationView{
             VStack {
-                VStack {    // Washer No.1
-                    Text("1번 세탁기")
-                    Text("\(viewModel.state1)")
-                }
-                .foregroundColor(.green)
-                .multilineTextAlignment(.center)
-                .padding()
-                .border(Color.black)
-                VStack {    // Washer No.2
-                    Text("2번 세탁기")
-                    Text("\(viewModel.state2)")
-                }
-                .foregroundColor(.blue)
-                .multilineTextAlignment(.center)
-                .padding()
-                .border(Color.black)
-            }.padding()
-            Spacer()
-            Button(action: {
-                isFixed1.toggle()
-                viewModel.state1 = "미사용"
-            }) {
-                HStack{
-                    if isFixed1 {
-                        Text("1번 세탁기 이상 없음")
-                            .padding()
-                            .frame(width: 300)
-                            .border(Color.black)
+                VStack {
+                    VStack {    // Washer No.1
+                        Text("1번 세탁기")
+                        Text("\(viewModel.state1)")
                     }
-                    else {
-                        Text("1번 세탁기 수리 필요")
-                            .foregroundColor(Color.black)
-                            .padding()
-                            .frame(width: 300)
-                            .background(Color.red)
-                            .border(Color.black)
+                    .foregroundColor(.green)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(Color.black)
+                    VStack {    // Washer No.2
+                        Text("2번 세탁기")
+                        Text("\(viewModel.state2)")
                     }
+                    .foregroundColor(.blue)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .border(Color.black)
+                }.padding()
+                Spacer()
+                Button(action: {
+                    isFixed1.toggle()
+                    viewModel.state1 = "미사용"
+                }) {
+                    HStack{
+                        if isFixed1 {
+                            Text("1번 세탁기 이상 없음")
+                                .padding()
+                                .frame(width: 300)
+                                .border(Color.black)
+                        }
+                        else {
+                            Text("1번 세탁기 수리 필요")
+                                .foregroundColor(Color.black)
+                                .padding()
+                                .frame(width: 300)
+                                .background(Color.red)
+                                .border(Color.black)
+                        }
+                    }
+                }
+                .disabled(isFixed1)
+                Button(action: {
+                    isFixed2.toggle()
+                    viewModel.state2 = "미사용"
+                }) {
+                    HStack{
+                        if isFixed2 {
+                            Text("2번 세탁기 이상 없음")
+                                .padding()
+                                .frame(width: 300)
+                                .border(Color.black)
+                        }
+                        else {
+                            Text("2번 세탁기 수리 필요")
+                                .foregroundColor(Color.black)
+                                .padding()
+                                .frame(width: 300)
+                                .background(Color.red)
+                                .border(Color.black)
+                        }
+                    }
+                }
+                .disabled(isFixed2)
+                NavigationLink(destination: AdminView()
+                                .navigationBarHidden(true)
+                                .navigationBarBackButtonHidden(true)){
+                    Text("완료")
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .frame(width: 300)
+                        .background(Color.black)
+                        .border(Color.black)
                 }
             }
-            .disabled(isFixed1)
-            Button(action: {
-                isFixed2.toggle()
-                viewModel.state2 = "미사용"
-            }) {
-                HStack{
-                    if isFixed2 {
-                        Text("2번 세탁기 이상 없음")
-                            .padding()
-                            .frame(width: 300)
-                            .border(Color.black)
-                    }
-                    else {
-                        Text("2번 세탁기 수리 필요")
-                            .foregroundColor(Color.black)
-                            .padding()
-                            .frame(width: 300)
-                            .background(Color.red)
-                            .border(Color.black)
-                    }
-                }
-            }
-            .disabled(isFixed2)
-            Spacer()
         }
     }
 }
