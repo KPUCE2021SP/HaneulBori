1fbede56e1ed34837d8e62f8dcf5e855e8a0fb40
hhh-one <904lhw@naver.com>
2021-08-06 14:40:35 +0900

    facebook, naver sns 로그인 연동

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/GlobalApplication.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/GlobalApplication.kt
@@ -1,13 +1,21 @@
 package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
 import android.app.Application
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsConstants
+import com.facebook.appevents.AppEventsLogger
 import com.kakao.sdk.common.KakaoSdk
+import com.nhn.android.naverlogin.OAuthLogin
+
 
 class GlobalApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, getString(R.string.kakao_app_key))
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+
     }
 
 }
--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/LoginActivity.kt
@@ -10,11 +10,34 @@ import kotlinx.android.synthetic.main.activity_login.*
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import android.widget.Button
+import com.facebook.CallbackManager
 import kotlinx.android.synthetic.main.activity_login.*
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsConstants
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+
+import com.facebook.login.LoginManager
+import com.facebook.AccessToken
+import com.nhn.android.naverlogin.OAuthLogin
+import java.util.*
+import android.widget.Toast
+import com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler
+
+import com.nhn.android.naverlogin.OAuthLoginHandler
+
+
+
 
 
 class LoginActivity : AppCompatActivity()
 {
+    var callbackManager = CallbackManager.Factory.create()
+    var mOAuthLoginModule: OAuthLogin = OAuthLogin.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -59,11 +82,68 @@ class LoginActivity : AppCompatActivity()
             }
         }
 
+        //페이스북 로그인
+        facebookBtn.setOnClickListener {
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"))
+            // 페이스북 로그인 콜백
+            LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult?> {
+                    override fun onSuccess(loginResult: LoginResult?) {
+                        Log.d("facebookLogin", "Success")
+                        var intent = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(intent)
+                    }
 
-        signInBtn.setOnClickListener {
-            var intent= Intent(applicationContext,MainActivity::class.java)
-            startActivity(intent)
+                    override fun onCancel() {
+                        Log.d("facebookLogin", "Cancel")
+                    }
+
+                    override fun onError(exception: FacebookException) {
+                        Log.d("facebookLogin", "Error")
+                    }
+                })
         }
 
+        // 사용자가 이미 페이스북에 로그인 했는지 확인(아직 미사용) -> 처리문 작성 필요
+        //val accessToken: AccessToken = AccessToken.getCurrentAccessToken()
+        //val isLoggedIn: Boolean = accessToken != null && !accessToken.isExpired
+
+        val mOAuthLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+            override fun run(success: Boolean) {
+                if (success) {
+                    val accessToken = mOAuthLoginModule.getAccessToken(this@LoginActivity)
+                    val refreshToken = mOAuthLoginModule.getRefreshToken(this@LoginActivity)
+                    val expiresAt = mOAuthLoginModule.getExpiresAt(this@LoginActivity)
+                    val tokenType = mOAuthLoginModule.getTokenType(this@LoginActivity)
+                    var intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val errorCode = mOAuthLoginModule.getLastErrorCode(this@LoginActivity).code
+                    val errorDesc = mOAuthLoginModule.getLastErrorDesc(this@LoginActivity)
+                    Toast.makeText(
+                        this@LoginActivity, "errorCode:" + errorCode
+                                + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
+        naverBtn.setOnClickListener {
+            // 네이버 로그인
+            mOAuthLoginModule.init(
+                this@LoginActivity
+                ,getString(R.string.naver_client_id)
+                ,getString(R.string.naver_client_secret)
+                ,getString(R.string.naver_client_name)
+            )
+            mOAuthLoginModule.startOauthLoginActivity(this, mOAuthLoginHandler)
+        }
+
+    }
+
+    // 로그인 결과를 callbackManager를 통해 LoginManager에 전달
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
