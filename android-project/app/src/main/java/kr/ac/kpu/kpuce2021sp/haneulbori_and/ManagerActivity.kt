71345219c32f1a5b6d69a04a49f5393d3bad2249
hkcho01 <hkcho01@naver.com>
2021-08-09 18:35:48 +0900

    Get Data from Firebase - working

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
@@ -4,27 +4,58 @@ import android.app.Activity
 import android.app.ActivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.res.ComplexColorCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_manager.*
 
 class ManagerActivity : AppCompatActivity()
 {
+    val db = FirebaseFirestore.getInstance()
+    val itemList = arrayListOf<ListLayout>()
+    val adapter = ListAdapter(itemList)
 
-    var db=FirebaseFirestore.getInstance()
-    var itemList= arrayListOf<ListLayout>()
-    var adapter=ListAdapter(itemList)
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_manager)
+        var binding = ActivityManager.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.rvList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvList.adapter = adapter
 
+        binding.btnRead.setOnClickListener {
+            db.collection("User")
+                .get()
+                .addOnSuccessListener { result ->
 
+                    itemList.clear()
+                    for (document in result) {
+                        val item = ListLayout(document["name"] as String, document["number"] as String)
+                        itemList.add(item)
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                .addOnFailureListener { exception ->
 
+                    Log.w("MainActivity", "Error getting documents: $exception")
+                }
+        }
     }
+
+
 }
