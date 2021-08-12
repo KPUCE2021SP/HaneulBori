967f0d08d9947d272720f1eea252435149ae09a4
hkcho01 <hkcho01@naver.com>
2021-08-12 19:17:39 +0900

    Get bookList from firebase - doing

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
 import android.app.Activity
 import android.app.ActivityManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.core.content.res.ComplexColorCompat.inflate
 import androidx.core.graphics.drawable.DrawableCompat.inflate
@@ -23,12 +25,15 @@ import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_manager.*
+import java.time.LocalDate
 
 class ManagerActivity : AppCompatActivity()
 {
     val db = FirebaseFirestore.getInstance()
     val itemList = arrayListOf<ListLayout>()
     val adapter = ListAdapter(itemList)
+    @RequiresApi(Build.VERSION_CODES.O)
+    var date= LocalDate.now()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +42,56 @@ class ManagerActivity : AppCompatActivity()
         rv_list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rv_list.adapter = adapter
 
-        btnRead.setOnClickListener {
+        btnReadAll.setOnClickListener {
             db.collection("User")
                 .get()
                 .addOnSuccessListener { result ->
                     itemList.clear()
                     for (document in result) {
-                        val item = ListLayout(document["Birthday"] as String, document["Email"] as String, document["Name"] as String, document["PhoneNumber"] as String, document["Sex"] as String)
+                        val item = ListLayout(document["Birthday"] as String, document["Email"] as String, document["Name"] as String, document["PhoneNumber"] as String, document["Sex"] as String, document["bookList"] as Array<String>)
                         itemList.add(item)
                     }
                     adapter.notifyDataSetChanged()
                 }
                 .addOnFailureListener { exception ->
                     Toast.makeText(applicationContext,"Fail",Toast.LENGTH_SHORT).show()
-                    Log.w("MainActivity", "Error getting documents: $exception")
+                    //Log.w("MainActivity", "Error getting documents: $exception")
+                }
+        }
+        btnReadM.setOnClickListener {
+            db.collection("User")
+                .get()
+                .addOnSuccessListener { result ->
+                    itemList.clear()
+                    for (document in result) {
+                        val item = ListLayout(document["Birthday"] as String, document["Email"] as String, document["Name"] as String, document["PhoneNumber"] as String, document["Sex"] as String, document["bookList"] as Array<String>)
+                        if(item.sex=="M")
+                            itemList.add(item)
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext,"Fail",Toast.LENGTH_SHORT).show()
+                }
+        }
+        btnToday.setOnClickListener {
+            db.collection("User")
+                .get()
+                .addOnSuccessListener { result ->
+                    itemList.clear()
+                    for (document in result) {
+                        val item = ListLayout(document["Birthday"] as String, document["Email"] as String, document["Name"] as String, document["PhoneNumber"] as String, document["Sex"] as String, document["bookList"] as Array<String>)
+                        for(i in item.bookList.indices)
+                        {
+                            if(item.bookList[i]==date.toString())
+                                itemList.add(item)
+                        }
+
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext,"Fail",Toast.LENGTH_SHORT).show()
                 }
         }
     }
