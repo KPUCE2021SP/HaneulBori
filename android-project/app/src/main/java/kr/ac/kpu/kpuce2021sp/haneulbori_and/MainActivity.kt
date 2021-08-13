32e58a7b423e44732ec810154abcd9901b3340ac
hedwig3798 <hedwig3798@gmail.com>
2021-08-13 17:56:53 +0900

    예약 시간 DB에 입력

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/MainActivity.kt
@@ -4,15 +4,20 @@ package kr.ac.kpu.kpuce2021sp.haneulbori_and
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.TabActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 class MainActivity : TabActivity()
@@ -24,7 +29,9 @@ class MainActivity : TabActivity()
     val laundries = arrayListOf<String>()
     val machines = arrayListOf<String>()
     var nowLaundry: String = ""
+    val user = Firebase.auth.currentUser
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n", "SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -77,11 +84,37 @@ class MainActivity : TabActivity()
                                             dlg.setPositiveButton("확인", null)
                                         } else {
                                             val temp = it["book"] as ArrayList<*>
-                                            if (temp[0] == null){
+                                            if (true){
                                                 dlg.setTitle("예약 가능 시간")
                                                 dlg.setMessage("예약 내역이 없습니다. 예약하시겠습니까?")
                                                 dlg.setNegativeButton("취소",null)
                                                 dlg.setPositiveButton("확인") { dialog, which ->
+                                                    if (user != null) {
+                                                        userCollectionRef.document(user.uid).get()
+                                                            .addOnSuccessListener {
+                                                                if (it["bookList"] == null){
+                                                                    Toast.makeText(this, "faild to access database", Toast.LENGTH_SHORT).show()
+                                                                } else {
+                                                                    val item = it["bookList"] as ArrayList<String>
+
+                                                                    item.add(convertDateNow())
+                                                                    DB.runTransaction {
+                                                                        userCollectionRef.document(user.uid).update("bookList", item)
+                                                                        laundryCollectionRef.document(nowLaundry).collection("machine")
+                                                                            .document("w1").get()
+                                                                            .addOnSuccessListener {
+                                                                                val bookList = it["book"] as ArrayList<String>
+                                                                                bookList.add(convertDateNow())
+                                                                                laundryCollectionRef.document(nowLaundry).collection("machine")
+                                                                                    .document("w1")
+                                                                                    .update("book", bookList)
+                                                                            }
+                                                                    }
+
+                                                                }
+                                                            }
+                                                    }
+
                                                     tv.text = "예약 ${it.id}"
                                                 }
                                             } else {
@@ -114,5 +147,9 @@ class MainActivity : TabActivity()
                 Log.w("MainActivity", "Error getting documents: ")
             }
 
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun convertDateNow() : String{
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
     }
 }
