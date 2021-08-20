183072172929712f49ed927ed8250a1f0b060f3b
hkcho01 <hkcho01@naver.com>
2021-08-20 16:39:51 +0900

    CheckError - working

--- android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
+++ android-project/app/src/main/java/kr/ac/kpu/kpuce2021sp/haneulbori_and/ManagerActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kpu.kpuce2021sp.haneulbori_and
 
 import android.app.Activity
 import android.app.ActivityManager
+import android.app.AlertDialog
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -26,6 +27,7 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_manager.*
 import java.time.LocalDate
+import kotlin.properties.Delegates
 import kotlin.collections.hashMapOf as hashMapOf
 
 class ManagerActivity : AppCompatActivity()
@@ -35,6 +37,8 @@ class ManagerActivity : AppCompatActivity()
     val adapter = ListAdapter(itemList)
     @RequiresApi(Build.VERSION_CODES.O)
     var date= LocalDate.now()
+    //var choiceItem : Int? = null
+    var errorList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -100,40 +104,56 @@ class ManagerActivity : AppCompatActivity()
                 }
         }
 
-        //세탁기 추가
-        btnAddWasher.setOnClickListener {
-            val data= hashMapOf(
-                "book" to ArrayList<String>(),
-                "reason" to "normal",
-                "state" to true,
-                "type" to "washer"
-            )
+        //기기 삭제
+        /*btnDeleteMachine.setOnClickListener {
             db.collection("laundry").document("12floor").collection("machine")
-                .add(data)
-                .addOnSuccessListener {
-                    Toast.makeText(applicationContext,"세탁기 추가 완료",Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(applicationContext,"세탁기 추가 실패",Toast.LENGTH_SHORT).show()
-                }
-        }
+                .delete()
+        }*/
 
-        //탈수기 추가
-        btnAddDrier.setOnClickListener {
-            val data= hashMapOf(
-                "book" to ArrayList<String>(),
-                "reason" to "normal",
-                "state" to true,
-                "type" to "drier"
-            )
-            db.collection("laundry").document("12floor").collection("machine")
-                .add(data)
-                .addOnSuccessListener {
-                    Toast.makeText(applicationContext,"탈수기 추가 완료",Toast.LENGTH_SHORT).show()
+        //기기 추가
+        btnAddMachine.setOnClickListener {
+            var choiceMachine = arrayOf("세탁기","건조기")
+            var dlg=AlertDialog.Builder(this@ManagerActivity)
+            dlg.setTitle("추가할 기기를 선택하세요")
+            dlg.setItems(choiceMachine){ dialog, which->
+                var choiceItem=which
+                if(choiceItem==0)
+                {
+                    val data= hashMapOf(
+                        "book" to ArrayList<String>(),
+                        "reason" to "normal",
+                        "state" to true,
+                        "type" to "washer"
+                    )
+                    db.collection("laundry").document("12floor").collection("machine")
+                        .add(data)
+                        .addOnSuccessListener {
+                            Toast.makeText(applicationContext,"세탁기 추가 완료",Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(applicationContext,"세탁기 추가 실패",Toast.LENGTH_SHORT).show()
+                        }
                 }
-                .addOnFailureListener {
-                    Toast.makeText(applicationContext,"탈수기 추가 실패",Toast.LENGTH_SHORT).show()
+                else if(choiceItem==1)
+                {
+                    val data= hashMapOf(
+                        "book" to ArrayList<String>(),
+                        "reason" to "normal",
+                        "state" to true,
+                        "type" to "drier"
+                    )
+                    db.collection("laundry").document("12floor").collection("machine")
+                        .add(data)
+                        .addOnSuccessListener {
+                            Toast.makeText(applicationContext,"건조기 추가 완료",Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(applicationContext,"건조기 추가 실패",Toast.LENGTH_SHORT).show()
+                        }
                 }
+            }
+            dlg.setNegativeButton("취소",null)
+            dlg.show()
         }
 
         //기기 상태 조회
@@ -150,12 +170,28 @@ class ManagerActivity : AppCompatActivity()
                             if(item.bookList[i].contains(date.toString()))
                                 itemList.add(item)
                         }
-
                     }
                     adapter.notifyDataSetChanged()
                 }
                 .addOnFailureListener { exception ->
                     Toast.makeText(applicationContext,"조회 실패",Toast.LENGTH_SHORT).show()
+                }
+        }
+
+        //고장 접수 확인
+        btnCheckError.setOnClickListener {
+            db.collection("Error")
+                .get()
+                .addOnSuccessListener { result ->
+                    errorList.clear()
+                    for (document in result) {
+                        var item1=document["errorMessage"] as String
+                        errorList.add(item1)
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext,"Fail",Toast.LENGTH_SHORT).show()
                 }
         }
     }
